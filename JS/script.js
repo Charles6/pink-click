92f787e9cb05973d9128ac97024e66c2793f864f
Charles Nguyen <cpnguy@gmail.com>
2017-02-21 23:04:58 -0800

    score output
    Changed score output to be bigger

--- JS/script.js
+++ JS/script.js
@@ -20,26 +20,26 @@ const addPoints = (arr,point) => {
 	arr[arr.length] = arr[arr.length-1] + point;
 	console.log(arr);
 	shopCheck(arr[arr.length-1]);
-	document.getElementById("display").innerHTML = arr[arr.length-1];
+	document.getElementById("display").innerHTML = "score: " + arr[arr.length-1];
 	document.getElementById("ppc").innerHTML = clickObj.clickValue;
 	if (clickObj.autoState == false) {
 		document.getElementById("cps").innerHTML = "N/A";
 	} else {
 		document.getElementById("cps").innerHTML = clickObj.autoClickValue/(clickObj.autoClickSpeed/1000);
 	};
-	
+
 }
 
 const shopCheck = (score) => {
 	if (score >= 20) {
 		if (clickObj.autoState == false) {
-			document.getElementById("auto").innerHTML = 
+			document.getElementById("auto").innerHTML =
 				"<div id='autoBuy' onclick='autoClick(clickLog)' style='color:magenta'>Buy for 10 pts</div>";
 		}
 	}
 	if (score >= 10){
 		if (clickObj.multiState == false){
-			document.getElementById("multi").innerHTML = 
+			document.getElementById("multi").innerHTML =
 				"<div id='multiBuy' onclick='multiClick(clickLog)' style='color:magenta'>Buy for 10 pts</div>";
 		}
 	}
@@ -48,7 +48,7 @@ const shopCheck = (score) => {
 const autoClick = (arr) => {
 	clickObj.autoState = true;
 	arr[arr.length] = arr[arr.length-1] - 10;
-	document.getElementById("display").innerHTML = arr[arr.length-1];
+	document.getElementById("display").innerHTML = "score: " + arr[arr.length-1];
 	setInterval(function() {
 		addPoints(clickLog,clickObj.autoClickValue);
 	},clickObj.autoClickSpeed);
@@ -58,7 +58,7 @@ const autoClick = (arr) => {
 const multiClick = (arr) => {
 	clickObj.multiState = true;
 	arr[arr.length] = arr[arr.length-1] - 10;
-	document.getElementById("display").innerHTML = arr[arr.length-1];
+	document.getElementById("display").innerHTML = "score: " + arr[arr.length-1];
 	clickObj.clickValue = 5;
 	document.getElementById("multi").innerHTML = "Sold Out";
 	document.getElementById("cps").innerHTML = clickObj.autoClickValue/(clickObj.autoClickSpeed/1000);
